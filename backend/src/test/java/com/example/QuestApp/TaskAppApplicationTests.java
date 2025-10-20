9d34a3565c9813c2cd9f4e02278d69d911ebbe78
Kristina Maneva <kristinamaneva10@gmail.com>
2025-10-20 21:31:04 +0200

    use fake data for testing

--- backend/src/test/java/com/example/QuestApp/TaskAppApplicationTests.java
+++ backend/src/test/java/com/example/QuestApp/TaskAppApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.QuestApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // <-- add this
 class TaskAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
